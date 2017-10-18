66eb38dd0570c6355239815b82f4966aa9f0018f
Denis Perevalov <perevalovds@gmail.com>
2017-10-18 23:02:23 +0500

    added thumb

--- addon_config.mk
+++ addon_config.mk
@@ -1,10 +1,6 @@
 meta:
-	ADDON_NAME = ofxKinectV2
-	ADDON_DESCRIPTION = An addon for the new Kinect For Windows V2 sensor
-	ADDON_AUTHOR = Theo Watson
+	ADDON_NAME = ofxKuKinectV2
+	ADDON_DESCRIPTION = A Kuflex's addon for Kinect V2 sensor
+	ADDON_AUTHOR = Kuflex
 	ADDON_TAGS = "kinect" "kinectv2" "libfreenect" "libfreenect2"
-	ADDON_URL = https://github.com/ofTheo/ofxKinectV2
-
-osx:
-	
-	ADDON_FRAMEWORKS = OpenCL
+	ADDON_URL = https://github.com/kuflex/ofxKuKinectV2
