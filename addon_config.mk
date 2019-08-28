62290b6ec23f63c1fdbe03881b9d631054361cef
perevalovds <perevalovds@gmail.com>
2019-08-28 22:01:53 +0500

    automated copy to bin libusb and turbojpeg

--- addon_config.mk
+++ addon_config.mk
@@ -1,6 +1,11 @@
 meta:
 	ADDON_NAME = ofxKuKinectV2
-	ADDON_DESCRIPTION = A Kuflex's addon for Kinect V2 sensor
-	ADDON_AUTHOR = Kuflex
+	ADDON_DESCRIPTION = Addon for Kinect V2 sensor
+	ADDON_AUTHOR = perevalovds
 	ADDON_TAGS = "kinect" "kinectv2" "libfreenect" "libfreenect2"
-	ADDON_URL = https://github.com/kuflex/ofxKuKinectV2
+	ADDON_URL = https://github.com/perevalovds/ofxKuKinectV2
+
+vs:	
+	# x64
+	#ADDON_LIBS += libs/...
+	ADDON_DLLS_TO_COPY += copy_to_bin/libusb-1.0.dll;copy_to_bin/turbojpeg.dll
